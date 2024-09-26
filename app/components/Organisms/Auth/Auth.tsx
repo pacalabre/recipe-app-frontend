fa49c67accc668b3b5c316097dd6c0736030f907
Paul <pacalabre@gmail.com>
2024-09-25 19:51:47 -0600

    Refactored auth form to use react form hooks

--- app/components/Organisms/Auth/Auth.tsx
+++ app/components/Organisms/Auth/Auth.tsx
@@ -4,6 +4,7 @@ import Input from "../../Atoms/Input/Input";
 import Button from "../../Atoms/Button/Button";
 import styles from "./Auth.module.css";
 import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
 import {
   registerUser,
   loginUser,
@@ -11,36 +12,47 @@ import {
   logoutUser,
 } from "@/app/services/auth-service";
 
+type Inputs = {
+  registerName: string;
+  registerUsername: string;
+  registerEmail: string;
+  registerPassword: string;
+  loginEmail: string;
+  loginPassword: string;
+};
+
 const AuthForm = () => {
-  const [registerName, setRegisterName] = useState("");
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [formToShow, setFormToShow] = useState("login");
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
+  const [formToShow, setFormToShow] = useState("register");
   const [loggedinUser, setLoggedinUser] = useState(null);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-  const register = async (event: React.MouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    try {
-      const response = await registerUser(
-        registerName,
-        registerUsername,
-        registerEmail,
-        registerPassword
-      );
-    } catch (error) {
-      console.log(`There was an error registering this user: ${error}`);
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log("data", data);
+    if (formToShow === "register") {
+      try {
+        const response = await registerUser(
+          data.registerName,
+          data.registerUsername,
+          data.registerEmail,
+          data.registerPassword
+        );
+      } catch (error) {
+        console.log(`There was an error registering this user: ${error}`);
+      }
     }
-  };
 
-  const login = async (event: React.MouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    try {
-      const response = await loginUser(loginEmail, loginPassword);
-    } catch (error) {
-      console.log(`There was an error while logging in the user: ${error}`);
+    if (formToShow === "login") {
+      try {
+        const response = await loginUser(data.loginEmail, data.loginPassword);
+      } catch (error) {
+        console.log(`There was an error while logging in the user: ${error}`);
+      }
     }
   };
 
@@ -65,63 +77,57 @@ const AuthForm = () => {
 
   return (
     <>
-      {formToShow === "register" ? (
-        <form className={styles.authForm}>
-          <h2>Register</h2>
-          <label>Name:</label>
-          <Input
-            onchange={(event: React.ChangeEvent<HTMLInputElement>) =>
-              setRegisterName(event.target.value)
-            }
-          />
-          <label>User Name</label>
-          <Input
-            onchange={(event: React.ChangeEvent<HTMLInputElement>) =>
-              setRegisterUsername(event.target.value)
-            }
-          />
-          <label>Email</label>
-          <Input
-            onchange={(event: React.ChangeEvent<HTMLInputElement>) =>
-              setRegisterEmail(event.target.value)
-            }
-          />
-          <label>Password</label>
-          <Input
-            inputType="password"
-            onchange={(event: React.ChangeEvent<HTMLInputElement>) =>
-              setRegisterPassword(event.target.value)
-            }
-          />
-          <p>
-            Already have an account?
-            <button onClick={() => setFormToShow("login")}>Login</button>
-          </p>
-          <Button onclick={register} label="register"></Button>
-        </form>
-      ) : (
-        <form className={styles.authForm}>
-          <h2>Login</h2>
-          <label>Email</label>
-          <Input
-            onchange={(event: React.ChangeEvent<HTMLInputElement>) =>
-              setLoginEmail(event.target.value)
-            }
-          />
-          <label>Password</label>
-          <Input
-            inputType="password"
-            onchange={(event: React.ChangeEvent<HTMLInputElement>) =>
-              setLoginPassword(event.target.value)
-            }
-          />
-          <p>
-            Don't have an account yet?
-            <button onClick={() => setFormToShow("register")}>Register</button>
-          </p>
-          <Button onclick={login} label="login"></Button>
-        </form>
-      )}
+      <form className={styles.authForm} onSubmit={handleSubmit(onSubmit)}>
+        {formToShow === "register" ? (
+          <>
+            <h2>Register</h2>
+            <label>Name:</label>
+            <input {...register("registerName")} />
+            <label>User Name</label>
+            <input type="text" {...register("registerUsername")} />
+            <label>Email</label>
+            <input {...register("registerEmail")} />
+            <label>Password</label>
+            <input type="password" {...register("registerPassword")} />
+            <p>
+              Already have an account?
+              <button
+                onClick={() => {
+                  reset();
+                  setFormToShow("login");
+                }}
+              >
+                Login
+              </button>
+            </p>
+            <button type="submit">Register</button>
+          </>
+        ) : (
+          <>
+            <h2>Login</h2>
+            <label>Email</label>
+            <input
+              placeholder="kurt@beAnz.com"
+              type="text"
+              {...register("loginEmail")}
+            />
+            <label>Password</label>
+            <input type="password" {...register("loginPassword")} />
+            <p>
+              Don't have an account yet?
+              <button
+                onClick={() => {
+                  reset();
+                  setFormToShow("register");
+                }}
+              >
+                Register
+              </button>
+            </p>
+            <button type="submit">Login</button>
+          </>
+        )}
+      </form>
       <Button onclick={getLoggedinUser} label="get user info"></Button>
       <Button onclick={logout} label="logout"></Button>
       <Link href="/">Home</Link>
