9f0dfe762a8ed7486e381e0acac57dfbbe6d8506
Paul <pacalabre@gmail.com>
2024-10-12 15:47:20 -0600

    Added a recipes page and service.

---
+++ app/recipes/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useEffect, useState } from "react";
+import { getAllRecipes } from "../services/recipe-service";
+
+
+const Receipes = () => {
+    const [recipes,setRecipes] = useState<any>([]);
+
+    const getRecipeList = async () => {
+        const response = await getAllRecipes();
+        setRecipes(response)
+      };
+
+    useEffect(()=>{
+        getRecipeList();
+    },[])
+
+    return(
+        <>
+            <h2>All Recipes</h2> 
+            {recipes.length > 0 ? (
+            recipes?.map((recipe: any) => (
+            <div>
+                {recipe.title}
+            </div>
+            ))
+            ) : (
+                <p>nothing to show</p>
+            )}
+        </>
+    )
+}
+
+export default Receipes;
---
+++ app/services/recipe-service.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const getAllRecipes = async () => {
+    try {
+        const {data, status} = await axios.get(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/recipes`,
+            {
+              headers: {
+                Accept: "application/json",
+              },
+            }
+        )
+        return data;
+    } catch(error) {
+        console.log(`There was an error ${error}`);
+    }
+}
+
+
+
