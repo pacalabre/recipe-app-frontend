6c252683ff52cdcd512a497c1e628181e72994ad
Paul <pacalabre@gmail.com>
2024-10-12 17:32:45 -0600

    Refactored auth services to try/catch blocks and fixed issue where getting the currently logged in user was broken.

--- app/services/auth-service.ts
+++ app/services/auth-service.ts
@@ -1,24 +1,28 @@
 import axios from "axios";
 
-export const registerUser = (
+export const registerUser = async (
   registerName: string,
   registerUsername: string,
   registerEmail: string,
   registerPassword: string
 ) => {
-  axios({
-    method: "POST",
-    data: {
-      name: registerName,
-      userName: registerUsername,
-      email: registerEmail,
-      password: registerPassword,
-    },
-    withCredentials: true,
-    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/register`,
-  }).then((res) => {
-    return res;
-  });
+  try {
+    const { data, status } = await axios.post<any>(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/register`,
+      {
+        name: registerName,
+        userName: registerUsername,
+        email: registerEmail,
+        password: registerPassword,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+    return { data, status };
+  } catch (error) {
+    console.log(`There was an error getting users: ${error}`);
+  }
 };
 
 export const loginUser = async (loginEmail: string, loginPassword: string) => {
@@ -28,6 +32,9 @@ export const loginUser = async (loginEmail: string, loginPassword: string) => {
       {
         email: loginEmail,
         password: loginPassword,
+      },
+      {
+        withCredentials: true,
       }
     );
     return { data, status };
@@ -36,22 +43,31 @@ export const loginUser = async (loginEmail: string, loginPassword: string) => {
   }
 };
 
-export const getLoggedinUserInfo = () => {
-  axios({
-    method: "GET",
-    withCredentials: true,
-    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/user`,
-  }).then((res) => {
-    return res;
-  });
+export const getLoggedinUserInfo = async () => {
+  try {
+    const { data, status } = await axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/user`,
+      {
+        withCredentials: true,
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(`There was an error getting logged in user info: ${error}`);
+  }
 };
 
-export const logoutUser = () => {
-  axios({
-    method: "POST",
-    withCredentials: true,
-    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/logout`,
-  }).then((res) => {
-    return res;
-  });
+export const logoutUser = async () => {
+  try {
+    const { data, status } = await axios.post(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/logout`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+    return { data, status };
+  } catch (error) {
+    console.log(`There was an error getting users: ${error}`);
+  }
 };
