00b5e3dfed5c2172ec628a522ca18b6424ca82fb
Paul <pacalabre@gmail.com>
2024-12-17 18:58:56 -0700

    Moved the error message inside of the input component.

--- app/components/Atoms/Input/Input.tsx
+++ app/components/Atoms/Input/Input.tsx
@@ -1,5 +1,6 @@
 import styles from "./Input.module.css";
-import { UseFormRegister } from "react-hook-form";
+import { FieldError, FieldErrors, UseFormRegister } from "react-hook-form";
+import { RegisterFormInputs } from "@/app/types/registerFormInputTypes";
 
 type Props = {
   inputType?: string;
@@ -7,16 +8,27 @@ type Props = {
   formField: string;
   rules?: object;
   label?: string;
+  errorMsg?: string;
 };
-const Input = ({ register, formField, inputType, rules, label }: Props) => {
+
+const Input = ({
+  register,
+  formField,
+  inputType,
+  rules,
+  label,
+  errorMsg,
+}: Props) => {
+  const isError = errorMsg ? styles.error : "";
   return (
-    <div className={styles.inputContainer}>
+    <div className={`${styles.inputContainer} ${isError}`}>
       <label className={styles.inputLabel}>{label}</label>
       <input
         type={inputType}
         className={styles.input}
         {...register(formField, rules)}
       />
+      {errorMsg && <p className={styles.errorMsg}>{errorMsg}</p>}
     </div>
   );
 };
