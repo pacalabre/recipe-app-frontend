bf06659c24bf4ace27814db3ddac05dcc264ddfb
Paul <pacalabre@gmail.com>
2024-12-18 18:50:21 -0700

    Styled button component

--- app/components/Atoms/Button/Button.tsx
+++ app/components/Atoms/Button/Button.tsx
@@ -1,15 +1,33 @@
-import "./Button.css";
+import styles from "./Button.module.css";
 
 type Props = {
-  onclick: React.MouseEventHandler<HTMLButtonElement>;
+  onclick?: React.MouseEventHandler<HTMLButtonElement>;
   label: string;
+  varient: string;
+  type?: string;
 };
 
 const Button = (props: Props) => {
+  let buttonVarient;
+  if (props.varient === "secondary") {
+    buttonVarient = styles.secondary;
+  } else if (props.varient === "tertiary") {
+    buttonVarient = styles.tertiary;
+  } else {
+    buttonVarient = styles.primary;
+  }
+
+  const buttonType = props.type === "submit" ? "submit" : "button";
   return (
-    <button onClick={props.onclick} className="button">
-      {props.label}
-    </button>
+    <div className={styles.buttonContainer}>
+      <button
+        type={buttonType}
+        onClick={props.onclick}
+        className={`${styles.button} ${buttonVarient}`}
+      >
+        {props.label}
+      </button>
+    </div>
   );
 };
 
