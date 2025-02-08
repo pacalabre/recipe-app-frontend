e11f2afc7d81735e1dea9e827a4fb490b0c67039
Paul <pacalabre@gmail.com>
2025-02-07 17:24:58 -0700

    Added react font awesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ import AppNav from "./components/Organisms/AppNav/AppNav";
 import { usePathname } from "next/navigation";
 import path from "path";
 import Footer from "./components/Molecules/Footer/Footer";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
 
 const inter = Inter({ subsets: ["latin"] });
 
