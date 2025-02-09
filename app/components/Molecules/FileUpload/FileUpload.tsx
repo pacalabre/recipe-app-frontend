de5e18c4fed634ace8bfb45e0da9b4fae72f41b1
Paul <pacalabre@gmail.com>
2025-02-09 14:58:16 -0700

    Added react-dropzone library to handle drag and drop uploading of files.

--- app/components/Molecules/FileUpload/FileUpload.tsx
+++ app/components/Molecules/FileUpload/FileUpload.tsx
@@ -2,23 +2,41 @@ import "./../../../globals.css";
 import styles from "./FileUpload.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect } from "react";
+import { useDropzone } from "react-dropzone";
 
-const FileUpload = () => {
+function FileUpload() {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const files = acceptedFiles.map((file) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
   return (
     <>
       <label className={styles.fileUploadLabel}>Upload Image</label>
-      <div className={styles.fileUploadContentContainer}>
-        <FontAwesomeIcon
-          icon={faImage}
-          className={styles.imageIcon}
-        ></FontAwesomeIcon>
-        <div className={styles.fileUploadButtonContainer}>
-          <p className={styles.fileUploadP}>Drag or drop an image or</p>
-          <input className={styles.fileUpload} type="file" />
+      <section className={styles.fileUploadContainer}>
+        <div {...getRootProps({ className: "dropzone" })}>
+          <div className={styles.fileUploadContent}>
+            <FontAwesomeIcon
+              icon={faImage}
+              className={styles.imageIcon}
+            ></FontAwesomeIcon>
+            <input {...getInputProps()} />
+            <p>Drag and drop image, or click to select files</p>
+          </div>
+          {files.length ? (
+            <aside>
+              <h4>Recipe Image</h4>
+              <ul>{files}</ul>
+            </aside>
+          ) : null}
         </div>
-      </div>
+      </section>
     </>
   );
-};
+}
 
 export default FileUpload;
