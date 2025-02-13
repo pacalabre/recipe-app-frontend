12d6c64b0e7356620d2dcd213868041a7cc3ecd6
Paul <pacalabre@gmail.com>
2025-02-12 20:03:13 -0700

    Added functionality if an image is uploaded to display it to the user. Also added an remove image button.

--- app/components/Molecules/FileUpload/FileUpload.tsx
+++ app/components/Molecules/FileUpload/FileUpload.tsx
@@ -4,8 +4,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage } from "@fortawesome/free-solid-svg-icons";
 import { useDropzone } from "react-dropzone";
 import axios from "axios";
+import { useState } from "react";
+import Button from "../../Atoms/Button/Button";
 
 function FileUpload() {
+  const [recipeImage, setRecipeImage] = useState("");
   const onDrop = (files: (string | Blob)[]) => {
     const data = new FormData();
     data.append("file", files[0]);
@@ -18,7 +21,8 @@ function FileUpload() {
         `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_ACCOUNT}/image/upload`,
         data
       )
-      .then((data) => {
+      .then((res) => {
+        setRecipeImage(res.data.secure_url);
         console.log("data", data);
       });
   };
@@ -38,20 +42,33 @@ function FileUpload() {
       <label className={styles.fileUploadLabel}>Upload Image</label>
       <section className={styles.fileUploadContainer}>
         <div {...getRootProps({ className: "dropzone" })}>
-          <div className={styles.fileUploadContent}>
-            <FontAwesomeIcon
-              icon={faImage}
-              className={styles.imageIcon}
-            ></FontAwesomeIcon>
-            <input {...getInputProps()} />
-            <p>Drag and drop image, or click to select files</p>
-          </div>
-          {files.length ? (
-            <aside>
-              <h4>Recipe Image</h4>
-              <ul>{files}</ul>
-            </aside>
-          ) : null}
+          {recipeImage ? (
+            <div
+              style={{
+                backgroundImage: `url(${recipeImage})`,
+              }}
+              className={styles.recipeImageContainer}
+            >
+              <Button
+                label="Remove Image"
+                varient="secondary"
+                onclick={() => {
+                  setRecipeImage("");
+                }}
+              ></Button>
+            </div>
+          ) : (
+            <div className={styles.fileUploadContent}>
+              <FontAwesomeIcon
+                icon={faImage}
+                className={styles.imageIcon}
+              ></FontAwesomeIcon>
+              <input {...getInputProps()} />
+              <p className={styles.fileUploadMessage}>
+                Drag and drop image, or click to select files
+              </p>
+            </div>
+          )}
         </div>
       </section>
     </>
