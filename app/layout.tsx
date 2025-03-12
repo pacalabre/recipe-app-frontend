7e29089fd98664bd5641dea9ad8f34aa812239bc
Paul <pacalabre@gmail.com>
2025-03-11 19:05:30 -0600

    Updated layout file so the nav has access to the user

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,16 +24,18 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
         />
       </head>
       <body className={inter.className}>
-        {pathname !== "/login" && <AppNav></AppNav>}
-        <UserProvider>{children}</UserProvider>
-        {pathname !== "/login" && <Footer></Footer>}
+        <UserProvider>
+          {pathname !== "/login" && <AppNav></AppNav>}
+          {children}
+          {pathname !== "/login" && <Footer></Footer>}
+        </UserProvider>
       </body>
     </html>
   );
