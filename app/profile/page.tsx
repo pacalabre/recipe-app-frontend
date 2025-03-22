bb5296a1d1a85a4e7990cfdf9c37ad4e6d6406ae
Paul <pacalabre@gmail.com>
2025-03-22 17:13:53 -0600

    Added loader to profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,6 +6,7 @@ import { getUserRecipes } from "../services/user-service";
 import styles from "./profile.module.css";
 import { Recipe } from "../types/recipeTypes";
 import Link from "next/link";
+import Loader from "../components/Atoms/Loader/Loader";
 const Profile = () => {
   const { user } = useUser();
   const [userRecipes, setUserRecipes] = useState<any>();
@@ -27,47 +28,55 @@ const Profile = () => {
   }, [user]);
 
   return (
-    <main className={styles.profileCard}>
-      <section className={styles.userDetails}>
-        <div className={styles.userInitalContainer}>
-          <p className="userInital">{initials}</p>
-        </div>
-        <div className={styles.userNameEmail}>
-          <p>
-            Name:<span className={styles.userName}>{user?.name}</span>
-          </p>
-          <p>Email:{user?.email}</p>
-        </div>
-      </section>
-      <section>
-        <h3>Your Recipes:</h3>
-        {userRecipes?.length ? (
-          userRecipes?.map((recipe: Recipe, index: number) => (
-            <Link
-              className={styles.recipe}
-              href={{
-                pathname: "/recipe",
-                query: { id: recipe._id },
-              }}
-            >
-              <div
-                style={{ backgroundImage: `url(${recipe?.image})` }}
-                className={styles.recipeImage}
-              ></div>
-              <div className={styles.recipeDetails}>
-                <div className={styles.recipeTitleSubtitle}>
-                  <h4 className={styles.recipeTitle}>{recipe.recipeName}</h4>
-                  <p className={styles.recipeSubtitle}>{recipe.subtitle}</p>
-                </div>
-                <div className={styles.difficultyTotalMakeTime}></div>
-              </div>
-            </Link>
-          ))
-        ) : (
-          <p>nothing to show</p>
-        )}
-      </section>
-    </main>
+    <>
+      {user && userRecipes ? (
+        <main className={styles.profileCard}>
+          <section className={styles.userDetails}>
+            <div className={styles.userInitalContainer}>
+              <p className="userInital">{initials}</p>
+            </div>
+            <div className={styles.userNameEmail}>
+              <p>
+                Name:<span className={styles.userName}>{user?.name}</span>
+              </p>
+              <p>Email:{user?.email}</p>
+            </div>
+          </section>
+          <section>
+            <h3>Your Recipes:</h3>
+            {userRecipes?.length ? (
+              userRecipes?.map((recipe: Recipe, index: number) => (
+                <Link
+                  className={styles.recipe}
+                  href={{
+                    pathname: "/recipe",
+                    query: { id: recipe._id },
+                  }}
+                >
+                  <div
+                    style={{ backgroundImage: `url(${recipe?.image})` }}
+                    className={styles.recipeImage}
+                  ></div>
+                  <div className={styles.recipeDetails}>
+                    <div className={styles.recipeTitleSubtitle}>
+                      <h4 className={styles.recipeTitle}>
+                        {recipe.recipeName}
+                      </h4>
+                      <p className={styles.recipeSubtitle}>{recipe.subtitle}</p>
+                    </div>
+                    <div className={styles.difficultyTotalMakeTime}></div>
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <p>nothing to show</p>
+            )}
+          </section>
+        </main>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 };
 
