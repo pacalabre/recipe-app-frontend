15426b8ac0bbd0cb5396b110033a9adaa482b283
Paul <pacalabre@gmail.com>
2025-04-07 17:03:33 -0600

    Added loader to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,22 +2,24 @@
 import AuthForm from "../components/Organisms/Auth/Auth";
 import { useRouter } from "next/navigation";
 import { useUser } from "./../contextApi/UserProvider";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Loader from "../components/Atoms/Loader/Loader";
 
 const Login = () => {
   const router = useRouter();
   const { user, setUser } = useUser();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (user) {
       router.push("/allrecipes");
+      return;
+    } else {
+      setIsLoading(false);
     }
   }, [user]);
-  return (
-    <>
-      <AuthForm />
-    </>
-  );
+
+  return <>{isLoading ? <Loader /> : <AuthForm />}</>;
 };
 
 export default Login;
