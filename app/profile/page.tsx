d776172066f96dc06f2bf1ef2c63056b145bc5d4
Paul <pacalabre@gmail.com>
2025-04-08 18:45:36 -0600

    Added favorite recipes to the user profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,7 +2,10 @@
 
 import { useEffect, useState } from "react";
 import { useUser } from "../contextApi/UserProvider";
-import { getUserRecipes } from "../services/user-service";
+import {
+  getUserRecipes,
+  getUserFavoriteRecipes,
+} from "../services/user-service";
 import { deleteRecipe } from "../services/recipe-service";
 import styles from "./profile.module.css";
 import { Recipe } from "../types/recipeTypes";
@@ -14,6 +17,7 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 const Profile = () => {
   const { user } = useUser();
   const [userRecipes, setUserRecipes] = useState<any>();
+  const [userFavoriteRecipes, setUserFavoriteRecipes] = useState<any>();
   const [initials, setInitials] = useState<string>("");
 
   const callGetUserRecipes = async (id: string) => {
@@ -21,12 +25,20 @@ const Profile = () => {
       const response = await getUserRecipes(id);
       setUserRecipes(response);
     }
-    console.log("recipes", userRecipes);
+  };
+
+  const callGetUserFavoriteRecipes = async (id: string) => {
+    if (id) {
+      const response = await getUserFavoriteRecipes(id);
+      setUserFavoriteRecipes(response);
+    }
+    console.log("favorite recipes", userFavoriteRecipes);
   };
 
   useEffect(() => {
     if (user) {
       callGetUserRecipes(user.id);
+      callGetUserFavoriteRecipes(user.id);
       setInitials(user.name.charAt(0));
     }
   }, [user]);
@@ -35,67 +47,109 @@ const Profile = () => {
     <>
       {user ? (
         <main className={styles.profileCard}>
-          <section className={styles.userDetails}>
-            <div className={styles.userInitalContainer}>
-              <p className="userInital">{initials}</p>
-            </div>
-            <div className={styles.userNameEmail}>
-              <p>
-                Name:<span className={styles.userName}>{user?.name}</span>
-              </p>
-              <p>Email:{user?.email}</p>
-            </div>
-          </section>
-          <section>
-            <h3>Your Recipes:</h3>
-            {userRecipes?.length ? (
-              userRecipes?.map((recipe: Recipe, index: number) => (
-                <div className={styles.userRecipe}>
-                  <Link
-                    className={styles.recipeLink}
-                    href={{
-                      pathname: "/recipe",
-                      query: { id: recipe._id },
-                    }}
-                  >
-                    <div
-                      style={{ backgroundImage: `url(${recipe?.image})` }}
-                      className={styles.recipeImage}
-                    ></div>
-                    <div className={styles.recipeDetails}>
-                      <div className={styles.recipeTitleSubtitle}>
-                        <h4 className={styles.recipeTitle}>
-                          {recipe.recipeName}
-                        </h4>
-                        <p className={styles.recipeSubtitle}>
-                          {recipe.subtitle}
-                        </p>
+          <div className={styles.profileContentContainer}>
+            <section className={styles.userDetails}>
+              <div className={styles.userInitalContainer}>
+                <p className="userInital">{initials}</p>
+              </div>
+              <div className={styles.userNameEmail}>
+                <p>
+                  Name:<span className={styles.userName}>{user?.name}</span>
+                </p>
+                <p>Email:{user?.email}</p>
+              </div>
+            </section>
+            <div className={styles.profileRecipesContainer}>
+              <section>
+                <h3>Your Recipes:</h3>
+                {userRecipes?.length
+                  ? userRecipes?.map((recipe: Recipe, index: number) => (
+                      <div className={styles.userRecipe}>
+                        <Link
+                          className={styles.recipeLink}
+                          href={{
+                            pathname: "/recipe",
+                            query: { id: recipe._id },
+                          }}
+                        >
+                          <div
+                            style={{ backgroundImage: `url(${recipe?.image})` }}
+                            className={styles.recipeImage}
+                          ></div>
+                          <div className={styles.recipeDetails}>
+                            <div className={styles.recipeTitleSubtitle}>
+                              <h4 className={styles.recipeTitle}>
+                                {recipe.recipeName}
+                              </h4>
+                              <p className={styles.recipeSubtitle}>
+                                {recipe.subtitle}
+                              </p>
+                            </div>
+                            <div
+                              className={styles.difficultyTotalMakeTime}
+                            ></div>
+                          </div>
+                        </Link>
+                        <button
+                          className={styles.deleteRecipeButton}
+                          onClick={async () => {
+                            try {
+                              const response = await deleteRecipe(recipe);
+                              if (response?.status === 200) {
+                                const recipes = await getUserRecipes(user.id);
+                                if (recipes) setUserRecipes(recipes);
+                              }
+                            } catch (error) {
+                              console.log(`There was an error: ${error}`);
+                            }
+                          }}
+                        >
+                          <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                        </button>
                       </div>
-                      <div className={styles.difficultyTotalMakeTime}></div>
-                    </div>
-                  </Link>
-                  <button
-                    className={styles.deleteRecipeButton}
-                    onClick={async () => {
-                      try {
-                        const response = await deleteRecipe(recipe);
-                        if (response?.status === 200) {
-                          const recipes = await getUserRecipes(user.id);
-                          if (recipes) setUserRecipes(recipes);
-                        }
-                      } catch (error) {
-                        console.log(`There was an error: ${error}`);
-                      }
-                    }}
-                  >
-                    <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
-                  </button>
-                </div>
-              ))
-            ) : (
-              <p>nothing to show</p>
-            )}
-          </section>
+                    ))
+                  : null}
+              </section>
+              <section>
+                <h3>Your Favorite Recipes:</h3>
+                {userFavoriteRecipes?.length
+                  ? userFavoriteRecipes?.map(
+                      (recipe: Recipe, index: number) => (
+                        <div className={styles.userRecipe}>
+                          <Link
+                            className={styles.recipeLink}
+                            href={{
+                              pathname: "/recipe",
+                              query: { id: recipe._id },
+                            }}
+                          >
+                            <div
+                              style={{
+                                backgroundImage: `url(${recipe?.image})`,
+                              }}
+                              className={styles.recipeImage}
+                            ></div>
+                            <div className={styles.recipeDetails}>
+                              <div className={styles.recipeTitleSubtitle}>
+                                <h4 className={styles.recipeTitle}>
+                                  {recipe.recipeName}
+                                </h4>
+                                <p className={styles.recipeSubtitle}>
+                                  {recipe.subtitle}
+                                </p>
+                              </div>
+                              <div
+                                className={styles.difficultyTotalMakeTime}
+                              ></div>
+                            </div>
+                          </Link>
+                        </div>
+                      )
+                    )
+                  : null}
+              </section>
+            </div>
+          </div>
         </main>
       ) : (
         <Loader />
