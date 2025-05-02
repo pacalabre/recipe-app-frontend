6ce0eac0248a14ca795298a5b45915f4c1b75123
Paul Calabrese <pacalabre@gmail.com>
2025-05-01 19:52:24 -0600

    Added addTag function to tag service

--- app/services/tag-service.ts
+++ app/services/tag-service.ts
@@ -16,18 +16,35 @@ export const getAllTags = async () => {
   }
 };
 
-export const getUsers = async () => {
+export const addTag = async (tagName: string) => {
   try {
-    const { data, status } = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/users`,
+    const { data, status } = await axios.post(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tags`,
+      { tagName: tagName },
       {
         headers: {
           Accept: "application/json",
         },
       }
     );
-    return data;
+    return { data, status };
   } catch (error) {
-    console.log(`There was an error getting users: ${error}`);
+    console.log(`There was an error creating a tag: ${error}`);
   }
 };
+//Not sure if this is here on accident, commenting out for now.
+// export const getUsers = async () => {
+//   try {
+//     const { data, status } = await axios.get(
+//       `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/users`,
+//       {
+//         headers: {
+//           Accept: "application/json",
+//         },
+//       }
+//     );
+//     return data;
+//   } catch (error) {
+//     console.log(`There was an error getting users: ${error}`);
+//   }
+// };
