ccd094e34034cc7af93b674568eb6770ddd37bfb
Paul Calabrese <pacalabre@gmail.com>
2025-06-07 19:26:49 -0600

    Updated head link to google fonts to resolve error.

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
